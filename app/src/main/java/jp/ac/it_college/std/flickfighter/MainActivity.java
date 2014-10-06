063c4a8dfcf5dd3182a23b96408179196a9549f3
s13012 <s13012@std.it-college.ac.jp>
2014-10-06 15:21:21 +0900

    戦闘画面からリザルト画面への画面遷移完了

--- app/src/main/java/jp/ac/it_college/std/flickfighter/MainActivity.java
+++ app/src/main/java/jp/ac/it_college/std/flickfighter/MainActivity.java
@@ -1,9 +1,11 @@
 package jp.ac.it_college.std.flickfighter;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 
 public class MainActivity extends Activity {
@@ -12,22 +14,10 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
     }
 
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
+    public void helloClicked(View view){
+        Intent intent = new Intent(this, BattleActivity.class);
+        startActivity(intent);
     }
 }
