f29debb402e9ffb195974cf50e345c536cffa299
s13012 <s13012@std.it-college.ac.jp>
2014-10-06 16:10:48 +0900

    ステージ選択画面→戦闘画面→リザルト画面への画面遷移完了

--- app/src/main/java/jp/ac/it_college/std/flickfighter/StageSelectActivity.java
+++ app/src/main/java/jp/ac/it_college/std/flickfighter/StageSelectActivity.java
@@ -1,12 +1,19 @@
 package jp.ac.it_college.std.flickfighter;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class StageSelectActivity extends Activity{
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_stageselect);
+    }
+
+    public void gotoBattle(View view){
+        Intent intent = new Intent(this, BattleActivity.class);
+        startActivity(intent);
     }
 }
