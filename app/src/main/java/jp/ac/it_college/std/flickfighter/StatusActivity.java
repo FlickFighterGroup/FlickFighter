57f0ade86889059283d0765f81352738a30dfb97
s13012 <s13012@std.it-college.ac.jp>
2014-10-27 14:06:00 +0900

    不要なImport文削除

--- app/src/main/java/jp/ac/it_college/std/flickfighter/StatusActivity.java
+++ app/src/main/java/jp/ac/it_college/std/flickfighter/StatusActivity.java
@@ -2,9 +2,7 @@ package jp.ac.it_college.std.flickfighter;
 
 import android.app.Activity;
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 
 public class StatusActivity extends Activity{
