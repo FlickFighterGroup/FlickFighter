4dd11970db885188d001b070e3196e31cb40484d
s13012 <s13012@std.it-college.ac.jp>
2014-10-27 14:58:30 +0900

    SharedPreferencesでプレイヤーのステータスの初期値を設定

--- app/src/main/java/jp/ac/it_college/std/flickfighter/StatusActivity.java
+++ app/src/main/java/jp/ac/it_college/std/flickfighter/StatusActivity.java
@@ -2,13 +2,20 @@ package jp.ac.it_college.std.flickfighter;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 
 public class StatusActivity extends Activity{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        SharedPreferences getPlayerStatus = getSharedPreferences("player_status", MODE_PRIVATE);
+        int attack = getPlayerStatus.getInt("attack",1);
+        int defense = getPlayerStatus.getInt("defence",0);
+        int life = getPlayerStatus.getInt("life",3);
+        Log.v("attack", Integer.toString(attack));
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_status);
     }
