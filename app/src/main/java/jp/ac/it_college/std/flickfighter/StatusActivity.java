8af42ace741e8756d731859b88c9fe13e164293d
s13006 <s13006@std.it-college.ac.jp>
2014-10-27 18:01:41 +0900

    ステータス強化画面途中(1/3)

--- app/src/main/java/jp/ac/it_college/std/flickfighter/StatusActivity.java
+++ app/src/main/java/jp/ac/it_college/std/flickfighter/StatusActivity.java
@@ -1,6 +1,8 @@
 package jp.ac.it_college.std.flickfighter;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
@@ -9,21 +11,43 @@ import android.view.View;
 
 public class StatusActivity extends Activity{
 
+    //プレイヤーのステータスを取得
+    SharedPreferences getPlayerStatus = getSharedPreferences("player_status", MODE_PRIVATE);
+    int attack = getPlayerStatus.getInt("attack",1);
+    int defense = getPlayerStatus.getInt("defence",0);
+    int life = getPlayerStatus.getInt("life",3);
+    int point = getPlayerStatus.getInt("point",0);
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        SharedPreferences getPlayerStatus = getSharedPreferences("player_status", MODE_PRIVATE);
-        int attack = getPlayerStatus.getInt("attack",1);
-        int defense = getPlayerStatus.getInt("defence",0);
-        int life = getPlayerStatus.getInt("life",3);
         Log.v("attack", Integer.toString(attack));
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_status);
     }
 
-    public void upGrade(View v){
-        Intent intent = new Intent(this, UpGradeActivity.class);
-        startActivity(intent);
+    public void upGrade(String status_name, int status_point){
+        if (point >= 5){
+            AlertDialog.Builder alertDialog = new AlertDialog.Builder(this);
+            alertDialog.setTitle("確認");
+            alertDialog.setMessage("ポイントを５消費しますがよろしいですか？");
+
+            alertDialog.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                public void onClick(DialogInterface dialog, int which) {
+
+                }
+            });
+
+            alertDialog.setNegativeButton("CANCEL", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialogInterface, int i) {
+
+                }
+            });
+        }else{
+
+        }
     }
+
     public void stageSelect(View v){
         Intent intent = new Intent(this, StageSelectActivity.class);
         startActivity(intent);
