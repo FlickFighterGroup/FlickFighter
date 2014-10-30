b5fd7d5f26478050306f8c548060ed514d75e4e0
s13012 <s13012@std.it-college.ac.jp>
2014-10-30 18:47:15 +0900

    ステージセレクトレイアウトに全ステージのボタンとonClick実装

--- app/src/main/java/jp/ac/it_college/std/flickfighter/BattleActivity.java
+++ app/src/main/java/jp/ac/it_college/std/flickfighter/BattleActivity.java
@@ -3,6 +3,7 @@ package jp.ac.it_college.std.flickfighter;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -16,24 +17,8 @@ public class BattleActivity extends Activity {
         setContentView(R.layout.activity_battle);
     }
 
-    public void gotoResult(View view){
+    public void goToResult(View view){
         Intent intent = new Intent(this, ResultActivity.class);
         startActivity(intent);
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.battle, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
     }
 }
--- app/src/main/java/jp/ac/it_college/std/flickfighter/StageSelectActivity.java
+++ app/src/main/java/jp/ac/it_college/std/flickfighter/StageSelectActivity.java
@@ -6,14 +6,51 @@ import android.os.Bundle;
 import android.view.View;
 
 public class StageSelectActivity extends Activity{
+    public static final String STAGE_ID = "stageId";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_stageselect);
     }
 
-    public void gotoBattle(View view){
+    public void goToBattle(int stageId){
         Intent intent = new Intent(this, BattleActivity.class);
+        intent.putExtra(STAGE_ID, stageId);
         startActivity(intent);
+    }
+
+    public void stageSelect(View view) {
+        switch (view.getId()) {
+            case R.id.button_stage1:
+                goToBattle(1);
+                break;
+            case R.id.button_stage2:
+                goToBattle(2);
+                break;
+            case R.id.button_stage3:
+                goToBattle(3);
+                break;
+            case R.id.button_stage4:
+                goToBattle(4);
+                break;
+            case R.id.button_stage5:
+                goToBattle(5);
+                break;
+            case R.id.button_stage6:
+                goToBattle(6);
+                break;
+            case R.id.button_stage7:
+                goToBattle(7);
+                break;
+            case R.id.button_stage8:
+                goToBattle(8);
+                break;
+            case R.id.button_stage9:
+                goToBattle(9);
+                break;
+            case R.id.button_stage10:
+                goToBattle(10);
+                break;
+        }
     }
 }
