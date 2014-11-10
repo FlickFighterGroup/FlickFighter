131ca87bb7b5fed7c44a23934465966af85fdcc5
s13006 <s13006@std.it-college.ac.jp>
2014-11-10 16:19:54 +0900

    単語帳クラス作成、文字列をバトルアクティビティに表示まで実装。

---
+++ app/src/main/java/jp/ac/it_college/std/flickfighter/WordBook.java
@@ -0,0 +1,21 @@
+package jp.ac.it_college.std.flickfighter;
+
+class WordBook {
+    public WordBook(){
+
+    }
+    public static String[][] randomWord(){
+        String[][] word = new String[10][];
+        word[0] = new String[]{"あうあう","a2","a3","a4","a5","a6","a7","a8","a9","a0","a11","a12","a13","a14","a15"};
+        word[1] = new String[]{"a1","a2","a3","a4","a5","a6","a7","a8","a9","a0","a11","a12","a13","a14","a15"};
+        word[2] = new String[]{"a1","a2","a3","a4","a5","a6","a7","a8","a9","a0","a11","a12","a13","a14","a15"};
+        word[3] = new String[]{"a1","a2","a3","a4","a5","a6","a7","a8","a9","a0","a11","a12","a13","a14","a15"};
+        word[4] = new String[]{"a1","a2","a3","a4","a5","a6","a7","a8","a9","a0","a11","a12","a13","a14","a15"};
+        word[5] = new String[]{"a1","a2","a3","a4","a5","a6","a7","a8","a9","a0","a11","a12","a13","a14","a15"};
+        word[6] = new String[]{"a1","a2","a3","a4","a5","a6","a7","a8","a9","a0","a11","a12","a13","a14","a15"};
+        word[7] = new String[]{"a1","a2","a3","a4","a5","a6","a7","a8","a9","a0","a11","a12","a13","a14","a15"};
+        word[8] = new String[]{"a1","a2","a3","a4","a5","a6","a7","a8","a9","a0","a11","a12","a13","a14","a15"};
+        word[9] = new String[]{"a1","a2","a3","a4","a5","a6","a7","a8","a9","a0","a11","a12","a13","a14","a15"};
+        return word;
+    }
+}
