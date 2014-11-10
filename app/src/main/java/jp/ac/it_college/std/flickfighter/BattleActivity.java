b5b6d4e18dd423687d2341f68a453e296da1d18d
s13006 <s13006@std.it-college.ac.jp>
2014-11-10 16:47:30 +0900

    ステージセレクト後、STAGE_IDを取得し、ランダムで文字列を表示

--- app/src/main/java/jp/ac/it_college/std/flickfighter/BattleActivity.java
+++ app/src/main/java/jp/ac/it_college/std/flickfighter/BattleActivity.java
@@ -6,16 +6,17 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
+import android.widget.TextView;
+
 
 
 public class BattleActivity extends Activity {
     InputMethodManager inputMethodManager;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(new BattleLayout(this, null));
-
+        randomStringView();
         inputMethodManager = (InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
     }
 
@@ -26,5 +27,13 @@ public class BattleActivity extends Activity {
 
     public void keyBoardShown(View view) {
         inputMethodManager.toggleSoftInput(0, InputMethodManager.SHOW_IMPLICIT);
+    }
+
+    public void randomStringView(){
+
+        //敵の文字列を表示
+        TextView textView = (TextView) findViewById(R.id.enemyString);
+        textView.setText(WordBook.randomWordView(getIntent().getExtras().getInt(StageSelectActivity.STAGE_ID)));
+
     }
 }
--- app/src/main/java/jp/ac/it_college/std/flickfighter/WordBook.java
+++ app/src/main/java/jp/ac/it_college/std/flickfighter/WordBook.java
@@ -1,12 +1,14 @@
 package jp.ac.it_college.std.flickfighter;
 
+import java.util.Random;
+
 class WordBook {
     public WordBook(){
 
     }
-    public static String[][] randomWord(){
+    public static String[][] wordBook(){
         String[][] word = new String[10][];
-        word[0] = new String[]{"あうあう","a2","a3","a4","a5","a6","a7","a8","a9","a0","a11","a12","a13","a14","a15"};
+        word[0] = new String[]{"あうあう","あいあい","あい","あああ","いえ","いう","おいえ","おういえ","おい","あお","ええ","えい","ういお","おう","えいえいおー"};
         word[1] = new String[]{"a1","a2","a3","a4","a5","a6","a7","a8","a9","a0","a11","a12","a13","a14","a15"};
         word[2] = new String[]{"a1","a2","a3","a4","a5","a6","a7","a8","a9","a0","a11","a12","a13","a14","a15"};
         word[3] = new String[]{"a1","a2","a3","a4","a5","a6","a7","a8","a9","a0","a11","a12","a13","a14","a15"};
@@ -17,5 +19,11 @@ class WordBook {
         word[8] = new String[]{"a1","a2","a3","a4","a5","a6","a7","a8","a9","a0","a11","a12","a13","a14","a15"};
         word[9] = new String[]{"a1","a2","a3","a4","a5","a6","a7","a8","a9","a0","a11","a12","a13","a14","a15"};
         return word;
+    }
+
+    public static String randomWordView(int i){
+        Random rand = new Random();
+        String[][] tmp = wordBook();
+        return tmp[rand.nextInt(i)][rand.nextInt(15)];
     }
 }
