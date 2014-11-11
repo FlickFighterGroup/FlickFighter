34e7a21440841139db5418d015039aa0c0688a1f
s13012 <s13012@std.it-college.ac.jp>
2014-11-11 15:09:58 +0900

    文字列が一致しているかを判定するメソッド"charJudge"実装

--- app/src/main/java/jp/ac/it_college/std/flickfighter/BattleActivity.java
+++ app/src/main/java/jp/ac/it_college/std/flickfighter/BattleActivity.java
@@ -3,22 +3,29 @@ package jp.ac.it_college.std.flickfighter;
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.KeyEvent;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
+import android.widget.EditText;
 import android.widget.TextView;
 
 
 public class BattleActivity extends Activity {
     InputMethodManager inputMethodManager;
+    TextView enemyString;
+    EditText userInputText;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_battle);
         randomStringView();
         inputMethodManager = (InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
+        enemyString = (TextView) findViewById(R.id.enemyString);
+        userInputText = (EditText) findViewById(R.id.userInputText);
     }
 
     public void goToResult(View view){
@@ -31,23 +38,35 @@ public class BattleActivity extends Activity {
     }
 
     public void randomStringView(){
-
         //敵の文字列を表示
         TextView textView = (TextView) findViewById(R.id.enemyString);
         textView.setText(WordBook.randomWordView(getIntent().getExtras().getInt(StageSelectActivity.STAGE_ID)));
-
     }
 
     @Override
     public boolean dispatchKeyEvent(KeyEvent event) {
-        if (event.getKeyCode() == KeyEvent.KEYCODE_BACK) {
-            return super.dispatchKeyEvent(event);
+        if (event.getKeyCode() == KeyEvent.KEYCODE_ENTER) {
+            if (event.getAction() == KeyEvent.ACTION_DOWN) {
+                //Enterキーが押された時に文字列を判定する
+                charJudge(enemyString, userInputText);
+            }
+            return true;
         }
 
-        if (event.getKeyCode() == KeyEvent.KEYCODE_ENTER && event.getAction() == KeyEvent.ACTION_DOWN) {
-            //Judge
+        return super.dispatchKeyEvent(event);
+    }
+
+    private void charJudge(TextView enemyChar, EditText userText) {
+        if (enemyChar.getText().length() != userText.getText().length()
+                || !enemyChar.getText().toString().equals(userText.getText().toString())) {
+            //文字の長さと文字が一致しない場合
+            Log.v("charJudge", String.valueOf(false));
+            userText.setTextColor(Color.RED);
+            return;
         }
 
-        return true;
+        //文字が一致した場合
+        Log.v("charJudge", String.valueOf(true));
+        userText.setTextColor(Color.GREEN);
     }
 }
