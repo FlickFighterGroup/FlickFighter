25c1b5e1bbb6100bba48679e07658c8c615cba93
s13006 <s13006@std.it-college.ac.jp>
2015-01-19 15:07:40 +0900

    Result画面改良

--- app/src/main/java/jp/ac/it_college/std/flickfighter/ResultActivity.java
+++ app/src/main/java/jp/ac/it_college/std/flickfighter/ResultActivity.java
@@ -4,14 +4,51 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Gallery;
+import android.widget.ImageSwitcher;
+import android.widget.ImageView;
+import android.widget.ViewSwitcher;
 
 
-public class ResultActivity extends Activity {
-
+public class ResultActivity extends Activity implements ViewSwitcher.ViewFactory {
+    private int[] img =  {
+            R.drawable.true_img
+            ,R.drawable.false_img
+    };
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_result);
+        checkChallenge();
+    }
+
+    private void checkChallenge() {
+        ImageSwitcher challenge1 = (ImageSwitcher) findViewById(R.id.challenge_clear1);
+        challenge1.setFactory(this);
+        ImageSwitcher challenge2 = (ImageSwitcher) findViewById(R.id.challenge_clear2);
+        challenge2.setFactory((android.widget.ViewSwitcher.ViewFactory) this);
+        ImageSwitcher challenge3 = (ImageSwitcher) findViewById(R.id.challenge_clear3);
+        challenge3.setFactory((android.widget.ViewSwitcher.ViewFactory) this);
+
+        if (false) {
+            challenge1.setImageResource(img[0]);
+        } else {
+            challenge1.setImageResource(img[1]);
+        }
+
+        if (true) {
+            challenge2.setImageResource(img[0]);
+        } else {
+            challenge2.setImageResource(img[1]);
+        }
+
+        if (false) {
+            challenge3.setImageResource(img[0]);
+        } else {
+            challenge3.setImageResource(img[1]);
+        }
+
     }
 
 
@@ -32,5 +69,16 @@ public class ResultActivity extends Activity {
             return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public View makeView() {
+        // ApiDemos->Views->ImageSwitcherのソースからメソッドを丸々コピー
+        ImageView i = new ImageView(this);
+        i.setBackgroundColor(0xFF000000);
+        i.setScaleType(ImageView.ScaleType.FIT_CENTER);
+        i.setLayoutParams(new ImageSwitcher.LayoutParams(
+                Gallery.LayoutParams.MATCH_PARENT, Gallery.LayoutParams.MATCH_PARENT));
+        return i;
     }
 }
